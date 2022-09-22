3978ce97322b61782f7dcd75239f9f3d09b55eda
Jeffrey Zou <jzou30@stuy.edu>
2022-09-22 09:16:31 -0400

    added

---
+++ 04_choose/krewes.py
@@ -0,0 +1,15 @@
+ """Jeffrey Zou Ivan Yeung
+ SoftDev
+ K01
+ Krewes/Python dictionary/We are trying to write a python program that randomly selects a devo from a dictionary
+ 9/22/2022
+ Time spent:
+ BlOCK SUMMARY:
+ DISCO:
+ QCC:
+ OPS SUMMARY:"""
+ 
+ krewes = {2:[devo1,devo2,devo3], 7:[devo1,devo2,devo3], 8:[devo1,devo2,devo3]}
+ 
+ 
+ 
