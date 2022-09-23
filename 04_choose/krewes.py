ae0ee8429edae800d52d766dfe46cbed1981c629
Jeffrey Zou <jzou30@stuy.edu>
2022-09-23 08:57:18 -0400

    updated krewes

--- 04_choose/krewes.py
+++ 04_choose/krewes.py
@@ -1,15 +1,20 @@
- """Jeffrey Zou Ivan Yeung
- SoftDev
- K01
- Krewes/Python dictionary/We are trying to write a python program that randomly selects a devo from a dictionary
- 9/22/2022
- Time spent:
- BlOCK SUMMARY:
- DISCO:
- QCC:
- OPS SUMMARY:"""
+"""
+Jeffrey Zou Ivan Yeung
+SoftDev
+K01
+Krewes/Python dictionary/We are trying to write a python program that randomly selects a devo from a dictionary
+9/22/2022
+Time spent:
+BlOCK SUMMARY:
+DISCO:
+QCC:
+OPS SUMMARY:"""
  
- krewes = {2:[devo1,devo2,devo3], 7:[devo1,devo2,devo3], 8:[devo1,devo2,devo3]}
+import random as rng
+krewes = {2:["devo1","devo2","devo3"], 7:["devo1","devo2","devo3"], 8:["devo1","devo2","devo3"]}
+list = [2,7,8]
+key = rng.choice(list)
+dictionarylist = krewes[key]
  
  
  
---
+++ 04_choose/krewes1.py
@@ -0,0 +1,22 @@
+"""
+Jeffrey Zou Ivan Yeung
+SoftDev
+K01
+Krewes/Python dictionary/We are trying to write a python program that randomly selects a devo from a dictionary
+9/22/2022
+Time spent:
+BlOCK SUMMARY:
+DISCO:
+QCC:
+OPS SUMMARY:"""
+ 
+import random as rng
+krewes = {2:["NICHOLAS",  "ANTHONY",  "BRIAN",  "SAMUEL",  "JULIA",  "YUSHA",  "CORINA",  "CRAIG",  "FANG MIN",  "JEFF",  "KONSTANTIN",  "AARON",  "VIVIAN",  "AYMAN",  "TALIA",  "FAIZA",  "ZIYING",  "YUK KWAN",  "DANIEL",  "WEICHEN",  "MAYA",  "ELIZABETH",  "ANDREW",  "VANSH",  "JONATHAN",  "ABID",  "WILLIAM",  "HUI",  "ANSON",  "KEVIN",  "DANIEL",  "IVAN",  "JASMINE",  "JEFFREY"],
+          7:["DIANA",  "DAVID",  "SAM",  "PRATTAY",  "ANNA",  "JING YI",  "ADEN",  "EMERSON",  "RUSSELL",  "JACOB",  "WILLIAM",  "NADA",  "SAMANTHA",  "IAN",  "MARC",  "ANJINI",  "JEREMY",  "LAUREN",  "KEVIN",  "RAVINDRA",  "SADI",  "EMILY",  "GITAE",  "MAY",  "MAHIR",  "VIVIAN",  "GABRIEL",  "BRIANNA",  "JUN HONG",  "JOSEPH",  "MATTHEW",  "JAMES",  "THOMAS",  "NICOLE",  "Karen"],
+          8:["ALEKSANDRA",  "NAKIB",  "AMEER",  "HENRY",  "DONALD",  "YAT LONG",  "SEBASTIAN",  "DAVID",  "YUKI",  "SHAFIUL",  "DANIEL",  "SELENA",  "JOSEPH",  "SHINJI",  "RYAN",  "APRIL",  "ERICA",  "JIAN HONG",  "VERIT",  "JOSHUA",  "WILSON",  "AAHAN",  "GORDON",  "JUSTIN",  "MAYA",  "FAIYAZ",  "SHREYA",  "ERIC",  "JEFFERY",  "BRIAN",  "KEVIN",  "SAMSON",  "BRIAN",  "HARRY",  "wanying"]}
+list = [2,7,8]
+key = rng.choice(list)
+dictionarylist = krewes[key]
+print(rng.choice(dictionarylist))
+ 
+ 
