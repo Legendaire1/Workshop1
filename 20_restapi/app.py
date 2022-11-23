4b35f28a40d6ed39e89352bfdbbda1c625bf6e8e
Jeffrey Zou <jzou30@stuy.edu>
2022-11-22 23:49:00 -0500

    finished 20

--- 20_restapi/app.py
+++ 20_restapi/app.py
@@ -1,9 +1,32 @@
-from flask import flask
-from flask import render_template
-from flask import request
+# Balls (Weichen + Jeffrey)
+# SoftDev
+# K20: API STUFF
+# 2022-11-21
+# time spent: .5 hrs
+
+from flask import Flask, render_template
+import urllib3
+import json
 
 app = Flask(__name__)
+@app.route('/')
+def hello():
+    with open('key_nasa.txt') as f:
+        apikey = f.read() # retrieves API key
 
-@app.route('/image')
-def image():
-    
+    http = urllib3.PoolManager()
+    link = 'https://api.nasa.gov/planetary/apod?api_key=' + apikey
+
+    response = http.request('GET', link)
+    _data = response.data
+
+    print(_data)
+
+    image = json.loads(_data)["url"]
+    explanation = json.loads(_data)["explanation"]
+
+    return render_template("main.html", image = image, explanation = explanation)
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
